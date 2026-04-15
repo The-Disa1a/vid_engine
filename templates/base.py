84fb906e4b3b3510c79510301a35be84e2ee3545
Chamod Disala <chamoddisala41@gmail.com>
2026-04-15 12:56:19 +0530

    Update base.py

--- templates/base.py
+++ templates/base.py
@@ -1,4 +1,4 @@
 class BaseTemplate:
     """Interface for rendering video scenes."""
-    def make_scene(self, sentence_text, idx, res, kw=None):
+    def make_scene(self, sentence_text, idx, res, kw=None, global_meta=None):
         raise NotImplementedError("Each template must implement the make_scene method.")
